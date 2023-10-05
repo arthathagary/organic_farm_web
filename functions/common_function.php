b5fecc1a4caf92d2690169c8fa2098ad15e18caf
Arthath Agary <arthathagary@gmail.com>
2023-10-06 01:01:46 +0530

    add getLimitedBlogs,getLimitedUniqueCategory and getLimitedProducts functions

--- functions/common_function.php
+++ functions/common_function.php
@@ -41,11 +41,102 @@ function getProducts() {
                 }
 }
 
+
+function getLimitedProducts() {
+    global $con;
+    if(!isset($_GET['category'])){
+     $select_query = "SELECT * FROM products limit 8";
+                         $result_query = mysqli_query($con, $select_query);
+                         while ($row = mysqli_fetch_assoc($result_query)){
+                             $product_id = $row['product_id'];
+                             $product_name = $row['product_name'];
+                             $product_description = $row['product_description'];
+                             $category_id = $row['category_id'];
+                             $product_price = $row['product_price'];
+                             $product_image = $row['product_image'];
+                             $product_status = $row['product_status'];
+ 
+                            echo "<div class='col-xl-3 col-lg-4 col-md-6'>
+                            <div class='product-item'>
+                                <div class='position-relative bg-light overflow-hidden'>
+                                    <img class='img-fluid w-100' src='./admin/product_images/$product_image' alt=''>
+                                    <div class='bg-secondary rounded text-white position-absolute start-0 top-0 m-4 py-1 px-3'>New</div>
+                                </div>
+                                <div class='text-center p-4'>
+                                    <a class='d-block h5 mb-2' href=''>$product_name</a>
+                                    <span class='text-primary me-1'>$product_price</span>   
+                                </div>
+                                <div class='d-flex border-top'>
+                                    <small class='w-50 text-center border-end py-2'>
+                                        <a class='text-body' href=''><i class='fa fa-eye text-primary me-2'></i>View detail</a>
+                                    </small>
+                                    <small class='w-50 text-center py-2'>
+                                        <a class='text-body' href=''><i class='fa fa-shopping-bag text-primary me-2'></i>Add to cart</a>
+                                    </small>
+                                </div>
+                            </div>
+                        </div>";
+                        
+                     }
+                 }
+ }
+
+
+
+
 function getUniqueCategory() {
     global $con;
    if(isset($_GET['category'])){
     $category_id = $_GET['category'];
     $select_query = "SELECT * FROM `products` WHERE category_id = $category_id";
+                        $result_query = mysqli_query($con, $select_query);
+                        $num_of_rows = mysqli_num_rows($result_query);
+                        if($num_of_rows == 0){
+                            echo "<div class='col-lg-12'>
+                            <div class='alert alert-danger text-center' role='alert'>
+                                No product found in this category
+                            </div>";
+                        }else{
+                        while ($row = mysqli_fetch_assoc($result_query)){
+                            $product_id = $row['product_id'];
+                            $product_name = $row['product_name'];
+                            $product_description = $row['product_description'];
+                            $category_id = $row['category_id'];
+                            $product_price = $row['product_price'];
+                            $product_image = $row['product_image'];
+                            $product_status = $row['product_status'];
+
+                           echo "<div class='col-xl-3 col-lg-4 col-md-6'>
+                           <div class='product-item'>
+                               <div class='position-relative bg-light overflow-hidden'>
+                                   <img class='img-fluid w-100' src='./admin/product_images/$product_image' alt=''>
+                                   <div class='bg-secondary rounded text-white position-absolute start-0 top-0 m-4 py-1 px-3'>New</div>
+                               </div>
+                               <div class='text-center p-4'>
+                                   <a class='d-block h5 mb-2' href=''>$product_name</a>
+                                   <span class='text-primary me-1'>$product_price</span>   
+                               </div>
+                               <div class='d-flex border-top'>
+                                   <small class='w-50 text-center border-end py-2'>
+                                       <a class='text-body' href=''><i class='fa fa-eye text-primary me-2'></i>View detail</a>
+                                   </small>
+                                   <small class='w-50 text-center py-2'>
+                                       <a class='text-body' href=''><i class='fa fa-shopping-bag text-primary me-2'></i>Add to cart</a>
+                                   </small>
+                               </div>
+                           </div>
+                       </div>";
+                       
+                    }
+                }
+                }
+}
+
+function getLimitedUniqueCategory() {
+    global $con;
+   if(isset($_GET['category'])){
+    $category_id = $_GET['category'];
+    $select_query = "SELECT * FROM `products` WHERE category_id = $category_id limit 8";
                         $result_query = mysqli_query($con, $select_query);
                         $num_of_rows = mysqli_num_rows($result_query);
                         if($num_of_rows == 0){
@@ -127,3 +218,29 @@ function getBlogs(){
    </div>";
     }
 }
+
+function getLimitedBlogs(){
+    global $con;
+    $select_query = "SELECT * FROM `blogs` limit 3";
+    $result_query = mysqli_query($con, $select_query);
+    while ($row = mysqli_fetch_assoc($result_query)){
+        $blog_id = $row['blog_id'];
+        $blog_title = $row['blog_title'];
+        $blog_content = $row['blog_content'];
+        $blog_image = $row['blog_image'];
+        $blog_date = $row['blog_date'];
+        $who_post = $row['who_post'];
+        $blog_status = $row['blog_status'];
+       echo "<div class='col-lg-4 col-md-6 wow fadeInUp'>
+       <img class='img-fluid' src='./admin/product_images/$blog_image' alt=''>
+       <div class='bg-light p-4'>
+           <a class='d-block h5 lh-base mb-4' href=''>$blog_title</a>
+           <div class='text-muted border-top pt-4'>
+               <small class='me-3'><i class='fa fa-user text-primary me-2'></i>$who_post</small>
+               <small class='me-3'><i class='fa fa-calendar text-primary me-2'></i>$blog_date</small>
+           </div>
+       </div>
+   </div>";
+    }
+}
+
---
+++ include/navBar.php
@@ -0,0 +1,31 @@
+<div class="container-fluid fixed-top px-0">
+        <nav class="navbar navbar-expand-lg navbar-light py-lg-0 px-lg-5  " >
+            <a href="index.html" class="navbar-brand ms-4 ms-lg-0">
+                <h1 class="fw-bold text-primary m-0">Sus<span class="text-secondary">Agri</span></h1>
+            </a>
+            <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <div class="navbar-nav ms-auto p-4 p-lg-0">
+                <a href="index.php" class="nav-item nav-link">Home</a>
+                    <a href="about.php" class="nav-item nav-link">About Us</a>
+                    <a href="product.php" class="nav-item nav-link">Products</a>
+                    <a href="blog.php" class="nav-item nav-link">Blogs</a>
+                    </div>
+                    <a href="contact.php" class="nav-item nav-link">Contact Us</a>
+                </div>
+                <div class="d-none d-lg-flex ms-2">
+                    <a class="btn-sm-square bg-white rounded-circle ms-3" href="">
+                        <small class="fa fa-search text-body"></small>
+                    </a>
+                    <a class="btn-sm-square bg-white rounded-circle ms-3" href="">
+                        <small class="fa fa-user text-body"></small>
+                    </a>
+                    <a class="btn-sm-square bg-white rounded-circle ms-3" href="">
+                        <small class="fa fa-shopping-bag text-body"></small>
+                    </a>
+                </div>
+            </div>
+        </nav>
+    </div>
